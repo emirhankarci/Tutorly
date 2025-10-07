34b19fee30cdbcabf13ed9885225be17dfc28831
Emirhan Karcı <emirhankarci2@gmail.com>
2025-10-07 14:51:54 +0300

    feat: Set bottom navigation bar background color

--- app/src/main/java/com/emirhankarci/tutorly/presentation/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/emirhankarci/tutorly/presentation/ui/components/BottomNavigationBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
@@ -78,7 +79,9 @@ fun BottomNavigationBar(
         ).filterNotNull().toSet()
     }
 
-    NavigationBar {
+    NavigationBar(
+        containerColor = Color(0xFFFAFAFA)
+    ) {
         bottomNavItems.forEach { item ->
             val isSelected = remember(currentRoute) {
                 when (item.route) {
